66f46c9e664fb2b74f188d849b755456100c87e4
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2015-12-14 01:14:41 +0100

    Adds context initiliser

--- ComunioLite.Backend/ComunioLite.Backend.DAL/ComunioLiteContextInitialiser.cs
+++ ComunioLite.Backend/ComunioLite.Backend.DAL/ComunioLiteContextInitialiser.cs
@@ -1,8 +1,18 @@
 ﻿using System.Data.Entity;
+using ComunioLite.Backend.DAL.Seeds;
 
 namespace ComunioLite.Backend.DAL
 {
     public class ComunioLiteContextInitialiser : DropCreateDatabaseAlways<ComunioLiteContext>
     {
+        protected override void Seed(ComunioLiteContext context)
+        {
+            CategorySeed.GenerateSeedDataForCategory(context);
+            PlayerSeed.GenerateSeedDataForPlayer(context);
+            ManagerSeed.GenerateSeedDataForManager(context);
+            TeamSeed.GenerateSeedDataForTeam(context);
+
+            base.Seed(context);
+        }
     }
 }
