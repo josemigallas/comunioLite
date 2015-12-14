c0e2c0cde9ccb0830b99c5d66d829388a5a891ea
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2015-12-14 02:05:49 +0100

    Adds test runner for repository

--- ComunioLite.Backend/ComunioLite.Backend.Repository/Program.cs
+++ ComunioLite.Backend/ComunioLite.Backend.Repository/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace ComunioLite.Backend.Repository
+﻿using System;
+using ComunioLite.Backend.DAL;
+
+namespace ComunioLite.Backend.Repository
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var repository = new ComunioLiteRepository(new ComunioLiteContext());
+
+            using (repository)
+            {
+                foreach (var player in repository.GetPlayersInMarket())
+                {
+                    Console.WriteLine($"{player.Name}\t{player.Price}");
+                }
+            }
         }
     }
 }
