621d3f42be957ed29091fcc7f2e05f45cb62d049
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2016-01-07 19:47:48 +0100

    Refactor

--- ComunioLite.Backend/ComunioLite.BackEnd.TestRunners.ManagerLogin/Program.cs
+++ ComunioLite.Backend/ComunioLite.BackEnd.TestRunners.ManagerLogin/Program.cs
@@ -9,55 +9,87 @@ namespace ComunioLite.BackEnd.TestRunners.ManagerLogin
 {
     public class Program
     {
+        private static ComunioLiteRepository _repository;
+        private static IList<Manager> _managers;
+        private static string _playerName;
+
         public static void Main(string[] args)
         {
-            var repository = new ComunioLiteRepository(new ComunioLiteContext());
+            _repository = new ComunioLiteRepository(new ComunioLiteContext());
+            _managers = _repository.GetManagers() as IList<Manager>;
 
-            using (repository)
+            using (_repository)
             {
                 Console.WriteLine("¡Welcome to ComunioLite!");
                 Console.WriteLine();
 
-                var managers = repository.GetManagers() as IList<Manager>;
+                PrintManagersTable();
 
-                Console.WriteLine("Manager\t\tTeam");
-                Console.WriteLine();
-                foreach (var manager in managers.Where(m => m.Id != ComputerId))
-                {
-                    Console.WriteLine($"{manager.Name}\t\t{manager.Team.Name}");
-                }
-
-                Console.WriteLine();
                 Console.Write("Enter your name: ");
-                var playerName = Console.ReadLine();
+                _playerName = Console.ReadLine();
 
-                if (managers.All(m => !m.Name.Equals(playerName)))
+                if (PlayerNameNotExist())
                 {
-                    Console.Write("There is no manager with that name. ¿Create a new one <y/n>? ");
-                    var c = Console.ReadKey().KeyChar;
-                    Console.WriteLine();
-
-                    if (c == 'y')
-                    {
-                        Console.Write("Enter the name of your new team: ");
-                        var teamName = Console.ReadLine();
-
-                        var team = new Team
-                        {
-                            Name = teamName
-                        };
-
-                        var manager = new Manager
-                        {
-                            Name = playerName,
-                            Team = team,
-                            Money = StartingMoney
-                        };
-
-                        repository.AddManager(manager);
-                    }
+                    AskToCreateNewManager();
                 }
             }
+        }
+
+        private static void AskToCreateNewManager()
+        {
+            Console.Write("There is no manager with that name. ¿Create a new one <y/n>? ");
+            var c = Console.ReadKey().KeyChar;
+
+            switch (c)
+            {
+                case 'y':
+                    CreateNewManagerAndTeam();
+                    break;
+                case 'n':
+                    break;
+                default:
+                    AskToCreateNewManager();
+                    break;
+            }
+        }
+
+        private static void CreateNewManagerAndTeam()
+        {
+            Console.WriteLine();
+            Console.Write("Enter the name of your new team: ");
+            var teamName = Console.ReadLine();
+
+            var team = new Team
+            {
+                Name = teamName
+            };
+
+            var manager = new Manager
+            {
+                Name = _playerName,
+                Team = team,
+                Money = StartingMoney
+            };
+
+            _repository.AddManager(manager);
+        }
+
+        private static bool PlayerNameNotExist()
+        {
+            return _managers.All(m => !m.Name.Equals(_playerName));
+        }
+
+        private static void PrintManagersTable()
+        {
+            Console.WriteLine("Manager\t\tTeam");
+            Console.WriteLine();
+
+            foreach (var manager in _managers.Where(m => m.Id != ComputerId))
+            {
+                Console.WriteLine($"{manager.Name}\t\t{manager.Team.Name}");
+            }
+
+            Console.WriteLine();
         }
     }
 }
