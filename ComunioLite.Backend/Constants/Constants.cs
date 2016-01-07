d37d4e4a75c5f2df597e0ddac60b7f1cb4c7c087
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2016-01-07 20:40:59 +0100

    Fixes constants declaration in Constants project

--- ComunioLite.Backend/Constants/Constants.cs
+++ ComunioLite.Backend/Constants/Constants.cs
@@ -3,25 +3,25 @@
     public static class Constants
     {
         // Game Constants
-        public static double StartingMoney = 1000000;
+        public const double StartingMoney = 1000000;
 
         // Data Base Commons
-        public static string DbName = "ComunioLite";
+        public const string DbName = "ComunioLite";
 
         // Computer Player
-        public static int ComputerId = 1;
-        public static string TeamMarket = "Market";
+        public const int ComputerId = 1;
+        public const string TeamMarket = "Market";
 
         // Table Names
-        public static string TableCategory = "Category";
-        public static string TableManager = "Manager";
-        public static string TablePlayer = "Player";
-        public static string TableTeam = "Team";
+        public const string TableCategory = "Category";
+        public const string TableManager = "Manager";
+        public const string TablePlayer = "Player";
+        public const string TableTeam = "Team";
 
         // Column Configurations
-        public static int CategoryNameMaxLength = 20;
-        public static int ManagerNameMaxLength = 50;
-        public static int PlayerNameMaxLength = 50;
-        public static int TeamNameMaxLength = 50;
+        public const int CategoryNameMaxLength = 20;
+        public const int ManagerNameMaxLength = 50;
+        public const int PlayerNameMaxLength = 50;
+        public const int TeamNameMaxLength = 50;
     }
 }
